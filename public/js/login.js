868e3816cb6cf20249cc8dead1b4086ddb18b63a
Ashok Nayak <ashok.nayakii@gmail.com>
2022-04-01 17:12:12 -0400

    login js changed to accomodate signup data

--- public/js/login.js
+++ public/js/login.js
@@ -22,14 +22,16 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
+  const fullname = document.querySelector('#name-signup').value.trim();
+  const address = document.querySelector('#home-address').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
+  const dateOfBirth = document.querySelector('#age-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
+  if (fullname && address && email && dateOfBirth && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ fullname, address, email, dateOfBirth, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
