e418a9b4a59f326eed9abb840ea1d76ac9e9e2a5
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-04-05 19:37:47 -0400

    Developing front-end logic for transporting create-party modal inputs into POST fetch request

--- public/js/party-buttons.js
+++ public/js/party-buttons.js
@@ -1,31 +1,9 @@
 
 async function editPartyHandler(event) {
     event.preventDefault();
-    // We need to POST the email, and password from the
-    // form to our server, so go ahead and grab the data 
-    // from the form.
-    console.log('inside loginFormHandler');
-    const email = document.getElementById('email-login').value.trim();
-    const password = document.getElementById('password-login').value.trim();
-    // Once we have the email and password, make a 
-    // fetch() POST request to the /api/users/logins
-    // make sure all fields have values
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('dashboard');
-      } else {
-        alert(response.statusText);
-      }
-    }
+    console.log('inside editPartyHandler');
+    window.alert('This buttons requires some work. Sorry!');
+
 }
 
 async function deletePartyHandler(event) {
